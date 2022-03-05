4ee1a93d9efdf687664031ddeb8955d171de04fa
Hugo Alexandre <halexandremfernandes@gmail.com>
2022-03-05 20:40:50 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        Crypto.encrypt("test");
+    }
+
+}
